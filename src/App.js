b21d07b4280ff5dadb45b6f4f2f870ba4c091646
KiraBonk <videostuff16@gmail.com>
2022-05-02 17:03:52 -0300

    Restructuring some code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
 import { Fragment } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Select from "react-select";
 
 import classes from "./App.module.css";
 import Header from "./components/Header/Header";
 import SpellList from "./components/Spells/SpellList";
+import Card from "./components/UI/Card";
+
+const options = [
+  { value: -1, label: "All" },
+  { value: 0, label: "Cantrip" },
+  { value: 1, label: "1" },
+  { value: 2, label: "2" },
+  { value: 3, label: "3" },
+  { value: 4, label: "4" },
+  { value: 5, label: "5" },
+  { value: 6, label: "6" },
+  { value: 7, label: "7" },
+  { value: 8, label: "8" },
+  { value: 9, label: "9" },
+];
 
 const App = () => {
+  const [spells, setSpells] = useState([]);
+  const [error, setError] = useState(null);
+  const [spellLevel, setSpellLevel] = useState(-1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const changeHandler = (selectedLevel) => {
+    setSpellLevel(selectedLevel.value);
+  };
+
+  const fetchSpellsHandler = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const arrayOfSpells = await axios.get(
+        "https://www.dnd5eapi.co/api/spells"
+      );
+
+      const arrayOfPromises = await Promise.allSettled(
+        arrayOfSpells.data.results.map((spellData) => {
+          return axios.get(`https://www.dnd5eapi.co${spellData.url}`);
+        })
+      );
+
+      setSpells(
+        arrayOfPromises.map((spellData) => {
+          return spellData.value.data;
+          
+        })
+      );
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchSpellsHandler();
+  }, []);
+
+  
+
   return (
     <div
       className={classes.style}
@@ -13,13 +72,20 @@ const App = () => {
         backgroundAttachment: "fixed",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
-        minHeight: "100vh"
+        minHeight: "100vh",
       }}
     >
       <Fragment>
         <Header />
         <main className={classes.test}>
-          <SpellList />
+          <section className={classes.spells}>
+            <Select options={options} onChange={changeHandler} />
+            <Card>
+              {!isLoading && <SpellList props={spells} />}
+              {isLoading && <p>Loading...</p>}
+            </Card>
+            {error}
+          </section>
         </main>
       </Fragment>
     </div>
---
+++ src/components/Spells/SpellListCopy.js
@@ -0,0 +1,82 @@
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import Select from "react-select";
+
+// import SpellItem from "./SpellItem";
+// import classes from "./SpellList.module.css";
+// import Card from "../UI/Card";
+
+// const options = [
+//   { value: -1, label: "All" },
+//   { value: 0, label: "Cantrip" },
+//   { value: 1, label: "1" },
+//   { value: 2, label: "2" },
+//   { value: 3, label: "3" },
+//   { value: 4, label: "4" },
+//   { value: 5, label: "5" },
+//   { value: 6, label: "6" },
+//   { value: 7, label: "7" },
+//   { value: 8, label: "8" },
+//   { value: 9, label: "9" },
+// ];
+
+// const SpellList = (props) => {
+//   const [spells, setSpells] = useState([]);
+//   const [error, setError] = useState(null);
+//   const [spellLevel, setSpellLevel] = useState(-1);
+
+//   const changeHandler = (selectedLevel) => {
+//     setSpellLevel(selectedLevel.value);
+//     // console.log(selectedLevel.value)
+//   };
+
+//   const fetchSpellsHandler = async () => {
+//     setError(null);
+//     try {
+//       const arrayOfSpells = await axios.get(
+//         "https://www.dnd5eapi.co/api/spells"
+//       );
+
+//       const arrayOfPromises = await Promise.allSettled(
+//         arrayOfSpells.data.results.map((spellData) => {
+//           return axios.get(`https://www.dnd5eapi.co${spellData.url}`);
+//         })
+//       );
+
+//       setSpells(
+//         arrayOfPromises.map((spellData) => {
+//           return spellData.value.data;
+//         })
+//       );
+//     } catch (error) {
+//       setError(error.message);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchSpellsHandler();
+//   }, []);
+
+//   return (
+//     <div>
+//       <section className={classes.spells}>
+//         <Select options={options} onChange={changeHandler} />
+//         <Card>
+//           <ul>
+//             {spells.map((spellData) => {
+//               const { level, index, name } = spellData;
+//               if (spellLevel === -1) {
+//                 return <SpellItem key={index} name={name} level={level} />;
+//               } else if (spellLevel === level) {
+//                 return <SpellItem key={index} name={name} level={level} />;
+//               }
+//             })}
+//           </ul>
+//         </Card>
+//         {error}
+//       </section>
+//     </div>
+//   );
+// };
+
+// export default SpellList;
